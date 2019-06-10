647887210930e292140637080627a10d72a5addf
Greg Dubrow <gdubrow@sfsu.edu>
2019-06-10 08:40:12 -0700

    update r script

--- data import cleaning and map testing.R
+++ data import cleaning and map testing.R
@@ -1,2 +1,9 @@
 ## working script to build dataframes and test maps
 ## output maps will be in rmd file
+
+# data needed:
+# lat-long data for CA K-12 and higher ed
+# BART, MUNI, and other transit stop locations to calculate school-transit distance
+# Income, education, ethnicity & other data from census, ACS, etc
+
+library(tidyverse)
